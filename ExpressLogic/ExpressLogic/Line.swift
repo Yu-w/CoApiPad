f4959dec16b68afca95ea56eda2d8757e571d01a
jennyqujy <jqu3@illinois.edu>
2017-03-08 23:39:33 -0600

    message

--- ExpressLogic/ExpressLogic/Line.swift
+++ ExpressLogic/ExpressLogic/Line.swift
@@ -12,20 +12,27 @@ import CoreGraphics
 class Line: NSObject {
     
     static func drawLine(in view: UIView, from start: CGPoint, to end: [CGPoint]) {
+        let circle = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 18.0, height: 18.0))
+        circle.center = start
+        circle.layer.cornerRadius = 9
+        circle.backgroundColor = UIColor.white
+        circle.clipsToBounds = true
+        view.addSubview(circle)
         
         let line = CAShapeLayer()
         let linePath = UIBezierPath()
         linePath.move(to: start)
 
         end.forEach { (p) in
-            linePath.addLine(to: CGPoint(x: start.x, y: p.y))
+            linePath.addLine(to: CGPoint(x: start.x, y: p.y-15))
+            linePath.addQuadCurve(to: CGPoint(x: start.x+15, y: p.y), controlPoint:CGPoint(x: start.x, y: p.y))
             linePath.addLine(to: p)
         }
         
         line.path = linePath.cgPath
         line.strokeColor = UIColor.white.cgColor
         line.fillColor = UIColor.clear.cgColor
-        line.lineWidth = 3
+        line.lineWidth = 6
         view.layer.addSublayer(line)
         
     }
